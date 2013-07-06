1bed0f7bf736cf508fcf6b122269e0ae40bc9fcc
ItsJonQ <itsjonq@gmail.com>
2013-07-05 21:03:40 -0400

    Added Chat - Needs Validation

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
-
 	// Defining Global Variables
 		theHeader = $('#header');
+		streamWrap = $('#stream-wrapper');
 		streamerList = $('#streamer-list');
 		streamArea = $('#livestream');
+		theSidebar = $('#sidebar');
 		streamWatching = $('#streamer-watching');
+		headerOffset = $('#header').height();
+		whOffset = 64;
 
 	// Defining Global Functions
-		twitch_hots = function() {
-			var twitch_hots_api = 'http://api.justin.tv/api/stream/list.json?meta_game=StarCraft%20II:%20Heart%20of%20the%20Swarm&limit=25&jsonp=?';
-			$.getJSON(twitch_hots_api, function(data) {
+		twitchHots = function() {
+			var twitchHots_api = 'http://api.justin.tv/api/stream/list.json?meta_game=StarCraft%20II:%20Heart%20of%20the%20Swarm&limit=25&jsonp=?';
+			$.getJSON(twitchHots_api, function(data) {
 				$.each(data,function(i, data) {
 					var user = data.channel.login;
 					var twitch_live_view_count = data.channel_count;
@@ -19,7 +22,7 @@ $(document).ready(function(){
 			});
 		}
 
-		twitch_embed = function(username, view_count, counter) {
+		twitchEmbed = function(username, view_count, counter) {
 			var start = '&auto_play=true';
 			var width = '1920';
 			var height = '1080';
@@ -27,13 +30,22 @@ $(document).ready(function(){
 			streamArea.append(twitch_stream);	
 		}
 
+		twitchChat = function() {
+			var user = streamWrap.data('twitch-user');
+			var chat_embed_code = '<div class="twitch-chat side-widget"><div class="chat-header widget-header"><span class="highlight-text">'+user+'</span> Twitch Chat</div><div class="widget-body"><iframe frameborder="0" scrolling="no" id="chat_embed" src="http://twitch.tv/chat/embed?channel='+user+'&amp;popout_chat=true" height="100%" width="300"></iframe></div></div>';
+			theSidebar.append(chat_embed_code);
+		}
+
 		streamInject = function(ele, user, title) {
 			streamerList.find('li').removeClass('selected');
 			ele.addClass('selected');
+			
 			streamArea.html('');
-			twitch_embed(user);	
+			twitchEmbed(user);	
+			streamWrap.attr('data-twitch-user', user);
+			streamWrap.attr('data-twitch-title', title);
 			// streamWatching.html('You Are Now Watching <span>'+user+'</span> <span class="title">'+title+'</span>');
-			theHeader.find('.watching').html('Now Watching <span class="streamer">'+user+'</span>: <span class="title">'+title+'</span>');
+			theHeader.find('.watching').html('Now Watching <span class="streamer highlight-text">'+user+'</span>: <span class="title">'+title+'</span>');
 			// streamWatching.fadeIn('fast').delay(2000).fadeOut('fast');
 		}
 
@@ -44,12 +56,14 @@ $(document).ready(function(){
 		streamerListScroll = function() {
 			var sh = streamerList.find('ul').height();
 			var wh = $(window).height();
-			streamerList.find('ul').height(wh-64);
+			// streamerList.find('ul').height(wh-whOffset);
 		}
+
+
 
 	// Execute Initial Functions on Page Load
 
-		twitch_hots();
+		twitchHots();
 		
 		$(window).load(function(){
 
@@ -59,7 +73,7 @@ $(document).ready(function(){
 				var title_first = first.data('stream-title');
 
 			// Embed the First User's Stream
-				first.addClass('selected');
+				first.addClass('selected').focus();
 				streamInject(first, user_first, title_first);
 			
 			streamerList.find('li').on('click', function(){
@@ -68,38 +82,60 @@ $(document).ready(function(){
 				streamInject($(this), user_ID, title);
 			});	
 
-
 			// Defining Key Press Functions
 
 				$(document).keydown(function(e) {
 
-					if(e.keyCode == 38) {
-						var prevUser = streamerList.find('li.selected').prev()
-						var userID = prevUser.data('user');
-						var title = prevUser.data('stream-title');
-						if(!streamerList.find('li.selected').is(':first-child')) {
-							streamInject(prevUser, userID, title);
+					// "Up" for Previous
+						if(e.keyCode == 38) {
+							var prevUser = streamerList.find('li.selected').prev()
+							var userID = prevUser.data('user');
+							var title = prevUser.data('stream-title');
+							if(!streamerList.find('li.selected').is(':first-child')) {
+								streamInject(prevUser, userID, title);
+							}
 						}
-					}
 
-					if(e.keyCode == 40) {
-						var nextUser = streamerList.find('li.selected').next();
-						var userID = nextUser.data('user');
-						var title = nextUser.data('stream-title');
-						if(!streamerList.find('li.selected').is(':last-child')) {
-							streamInject(nextUser, userID, title);
+					// "Down" for Next
+						if(e.keyCode == 40) {
+							var nextUser = streamerList.find('li.selected').next();
+							var userID = nextUser.data('user');
+							var title = nextUser.data('stream-title');
+							if(!streamerList.find('li.selected').is(':last-child')) {
+								streamInject(nextUser, userID, title);
+							}
 						}
-					}
 
-					if(e.keyCode == 39) {
-						streamerList.addClass('active');
-						streamerListScroll();
-					}
+					// "Right" to Show Stream Menu
+						if(e.keyCode == 39) {
+							streamerList.addClass('active');
+							streamerListScroll();
+						}
 
-					if(e.keyCode ==37) {
-						streamerList.removeClass('active scroll');
-						streamerList.find('ul').height('auto');
-					}
+					// "Left" to Hide Stream Menu
+						if(e.keyCode == 37) {
+							streamerList.removeClass('active scroll');
+							streamerList.find('ul').height('auto');
+						}
+
+					// "C" for Chat
+						if(e.keyCode == 67) {
+							var sa = 'sidebar-active';
+							var tc = $('.twitch-chat');
+							if(!streamWrap.hasClass(sa)) {
+								streamWrap.addClass(sa);
+								theSidebar.removeClass('hidden');
+								var wh = $(window).height() - headerOffset;
+								tc.find('iframe').height(wh);
+								if(!tc.length) {
+									twitchChat();
+								}
+							} else {
+								streamWrap.removeClass(sa);
+								theSidebar.addClass('hidden');	
+							}
+							
+						}
 
 					// if (e.keyCode == 83) {
 					// 	if(!$('#streamer-search').hasClass('show')) {
@@ -114,8 +150,7 @@ $(document).ready(function(){
 	// Defining Mouse Action Functions
 
 		streamArea.on('mouseenter',function(){
-			streamerList.removeClass('active scroll');
-			streamerList.find('ul').height('auto');
+			streamerList.removeClass('active');
 		});
 
 		streamerList.on('mouseenter',function(){
@@ -124,8 +159,7 @@ $(document).ready(function(){
 		});
 
 		streamerList.on('mouseleave',function(){
-			$(this).removeClass('active scroll');
-			streamerList.find('ul').height('auto');
+			$(this).removeClass('active');
 		});
 
 		$('.header-elements').find('.the-menu, .the-logo').on('mouseenter',function(){
