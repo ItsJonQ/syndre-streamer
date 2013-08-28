4f836f39ce58aae4b480cbd742806012f86dbe9e
ItsJonQ <itsjonq@gmail.com>
2013-08-28 17:37:23 -0400

    Added players

--- resources/js/script.js
+++ resources/js/script.js
@@ -251,13 +251,13 @@ $(document).ready(function(){
 		playerRace = function() {
 
 			// Terran Players
-				var terran = '.avilo, .azylis, .brentstarcraft, .coltertv, .colthestc, .demuslim, .dragon, .egxeno, .empiretvkas, .eric1826, .escgoody, .gamegene, .ganzi, .hobbiton, .htomario, .joemanstarcraft, .koibu, .liquidtaeja, .lillekanin, .luisggg, .mewby, .mfiaguz, .nathanias, .painuser, .quanticflo, .ruff13, .s2sound, .selectkr, .squishy88, .sterlingkolde, .tumescentpie, .xeloxy';
+				var terran = '.avilo, .azylis, .brentstarcraft, .coltertv, .colthestc, .demuslim, .doncroft, .dragon, .egxeno, .empiretvkas, .eric1826, .escgoody, .gamegene, .ganzi, .hobbiton, .htomario, .joemanstarcraft, .koibu, .liquidtaeja, .lillekanin, .luisggg, .mewby, .mfiaguz, .nathanias, .painuser, .psz07, .quanticflo, .ruff13, .s2sound, .selectkr, .shadye, .squishy88, .startalebomber, .sterlingkolde, .tristareagle, .tumescentpie, .xeloxy';
 
 			// Protoss Players
-				var protoss = '.artosis, .axeltoss, .colminigun, .crimson_sc2, .desrowfighting, .dreadnoughtt, .eghuk, .finalmastery, .fiveyearold, .followgrubby, .incontroltv, .istubby, .jurisc2, .jushyfruit, .justsimpletv, .kuroa1, .liquidhero,  .puckk, .oraseno_snowm, .naniwasc2, .rohdarkness, .sc2pal, .sc2sage, .schmuzi, .shew_tv, .slavismoon, .snykes, .tarrantius, .tetzui, .theshrimpzor, .tokisadafuz, .torkhots, .wayne379, .weedamins, .welmu1, .wolvesspacemarine, .whitera';
+				var protoss = '.90stardust, .artosis, .axeltoss, .colminigun, .crimson_sc2, .desrowfighting, .dreadnoughtt, .eghuk, .finalmastery, .fiveyearold, .followgrubby, .incontroltv, .istubby, .jurisc2, .jushyfruit, .justsimpletv, .kuroa1, .liquidhero,  .puckk, .oraseno_snowm, .naniwasc2, .rohdarkness, .sc2pal, .sc2sage, .schmuzi, .shew_tv, .slavismoon, .snykes, .tarrantius, .tetzui, .tffmrpink, .thegreekgods, .theshrimpzor, .tokisadafuz, .torkhots, .wayne379, .weedamins, .welmu1, .wolvesspacemarine, .whitera, .yoanm';
 
 			// Zerg Players
-				var zerg = '.armzi, .bexysc, .coolagebrothers, .conniech, .dimaga, .edgesc, .empiretvpeptar, .empiretvzerg, .grinkersstarcraft, .hurricane1234, .idrajit, .kawaiirice, .liquidsnute,  .liquidtlo, .massansc, .msspyte, .namshar, .najzmajs, .nenshoua, .rootcatz, .protech, .tilea, .viruswhite, .wiredguitars, .wwminiraser';
+				var zerg = '.armzi, .bexysc, .coolagebrothers, .conniech, .dimaga, .edgesc, .empiretvpeptar, .empiretvzerg, .grinkersstarcraft, .hurricane1234, .idrajit, .kawaiirice, .liquidsnute,  .liquidtlo, .massansc, .msspyte, .namshar, .najzmajs, .nenshoua, .rootcatz, .protech, .tefel, .tilea, .viruswhite, .wiredguitars, .wwminiraser';
 
 			streamerList.find('li').each(function(){
 				if($(this).is(terran)) {
